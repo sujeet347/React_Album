03e97fdd575870b0032c7262024901e8ab3b469c
sujeet347 <ersujeetsingh8@gmail.com>
2024-10-16 13:39:40 +0530

    Committing all changes

--- buybusy-1/src/Components/Loader/Loader.jsx
+++
@@ -1,9 +0,0 @@
-//GETTING STYLES
-import styles from "./Loader.module.css"
-
-const Loader = () => {
-    return <div className={styles.container}>
-                <span class={styles.loader}></span>
-            </div>
-}
-export default Loader;
